cc7f50d7605c20a54816d801649bc8d5ccedc43f
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-01 21:54:17 +0200

    feat: this add make tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,41 @@
+class Key {
+  constructor(private signature = Math.random()) {}
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door = false;
+  tenants: Array<Person> = [];
+  key: Key;
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person) {
+    if (this.door) this.tenants.push(person);
+  }
+}
+
+class MyHouse extends House {
+  constructor(public key: Key) {
+    super();
+  }
+
+  openDoor(key: Key): void {
+    if (key === this.key) this.door = true;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +44,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
